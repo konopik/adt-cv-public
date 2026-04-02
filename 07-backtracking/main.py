c0e5f48bee138bc8c66b3741852dbd9e64a0f570
Honza_Riegel <honzikriegel@gmail.com>
2026-04-02 13:31:43 +0200

    sudoku

--- 07-backtracking/main.py
+++ 07-backtracking/main.py
@@ -11,37 +11,79 @@ class SudokuSolver:
 
         # list of lists (rows)
         loaded_rows: list[list[int]] = []
-        # TODO implement loading of the file
+        #  implement loading of the file
 
         # convert nested list to numpy array
+        try:
+            with open(file_path,'r',encoding='utf8') as f:
+                linenum:list[int] = []
+                for line in f:
+                    numbs = line.split(';')
+                    for n in numbs:
+                        try:
+                            n_int = int(n)
+                        except ValueError:
+                            pass
+                        linenum.append(n_int)
+                loaded_rows.append(linenum)
+        except FileNotFoundError:
+            pass
+
+
         self.field = np.array(loaded_rows)
 
 
 
     def check_sequence(self, sequence: np.ndarray) -> bool:
-        return True
+        seen = set()
+        all_v = []
+        temp = sequence
+        temp = temp[temp != 0]
+        for n in temp:
+            seen.add(n)
+            all_v.append(n)
+        return len(seen) == len(all_v)
 
 
     def check_row(self, row_index: int) -> bool:
-        return False
+        return self.check_sequence(sequence=self.field[row_index,:])
 
     def check_column(self, column_index: int) -> bool:
-        return False
+        return self.check_sequence(sequence=self.field[:,column_index])
 
     def check_block(self, row_index: int, column_index: int) -> bool:
-        return False
+        row_s_i = (row_index//3) *3
+        collumn_s_i = (column_index//3) *3
+        return self.check_sequence(sequence=self.field[row_s_i:row_s_i+3,collumn_s_i:collumn_s_i+3])
 
 
     def check_one_cell(self, row_index: int , column_index: int) -> bool:
-        return False
+        return self.check_row(row_index=row_index) and self.check_column(column_index=column_index) and self.check_block(row_index=row_index,column_index=column_index)
 
     def get_empty_cell(self) -> tuple[int, int] | None:
         """ Gets the coordinates of the next empty field. """
+        for row in range(9):
+            for col in range(9):
+                if self.field[row,col] == 0:
+                    return (row,col)
         return None
 
     def solve(self) -> bool:
         """ Recursively solves the sudoku. """
+        empty_cell = self.get_empty_cell()
+        if not empty_cell:
+            
+            return True
+        row, col = empty_cell
+
+        for v in range(1,10):
+            self.field[row,col] = v
+            if self.check_one_cell(row_index=row,column_index=col) and self.solve():
+                return True
+        self.field[row,col] = 0
+
         return False
+
 
 
 
@@ -49,6 +91,13 @@ class SudokuSolver:
 
 def main() -> None:
     sudoku_solver = SudokuSolver()
+    sudoku_solver.load(file_path=os.path.join('07-backtracking','sudoku.csv'))
+    result = sudoku_solver.solve()
+    if result:
+        print('naleyeno')
+        print(sudoku_solver.field)
+    else:
+        print('nenaleyeno')
 
 if __name__ == "__main__":
     main()

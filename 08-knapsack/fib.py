f8e62e9c4813469e307d88bd0355bb49d29a9f14
Kuchar-J <Kuchar.Jakub.2zs@gmail.com>
2026-04-09 13:04:58 +0200

    lol

--- 08-knapsack/fib.py
+++ 08-knapsack/fib.py
@@ -13,7 +13,7 @@ def fib_cache(n: int) -> int:
     if n <= 1:
         return n
     return fib_cache(n - 1) + fib_cache(n - 2)
-
+#b
 def fib_mem(n: int, lookup: dict[int, int]) -> int:
     if n <= 1:
         return n
@@ -24,6 +24,16 @@ def fib_mem(n: int, lookup: dict[int, int]) -> int:
     return lookup[n]
 
 
+def fib_fast(n:int)->int | None:
+    if n<=1:
+        return n
+    fib_list=[0]*(n+1)
+    fib_list[0]=0
+    fib_list[1]=1
+    for i in range(2,n+1):
+        fib_list[i]=(fib_list[i-1]+fib_list[i-2])
+    return fib_list[n]
+
 def main() -> None:
     lookup: dict[int, int] = {}
 
@@ -32,9 +42,12 @@ def main() -> None:
     # a = 40 # za jak dlouho se asi dočkáme?
 
     measure_time(lambda: fib_cache(a), 100)
-    measure_time(lambda: fib_mem(a, lookup), 100)
-    measure_time(lambda: fib(a))
-
+    print(fib_cache(a))
+    measure_time(lambda: fib_mem(a, {}), 100)
+    print(fib_mem(a, lookup))
+    #measure_time(lambda: fib(a))
+    measure_time(lambda: fib_fast(a), 100)
+    print(fib_fast(a))
 
 if __name__ == "__main__":
     main()
